c0b4edab2310469f64669c96acdfd3d72c922ace
haivor <halvor.b.birkeland@haugnett.no>
2024-04-15 17:52:38 +0200

    fungerer å finne intersection noter, men i midten ehhh

--- detection lines/linedetect.py
+++ detection lines/linedetect.py
@@ -1,6 +1,28 @@
 import cv2
 import numpy as np 
 
+def line_circle_intersection(line_start, line_end, circle_center, circle_radius):
+    # Vector representation of the line
+    line_vec = np.array(line_end) - np.array(line_start)
+    
+    # Vector from line start to circle center
+    start_to_center = np.array(circle_center) - np.array(line_start)
+    
+    # Projection of start_to_center vector onto line_vec
+    projection = np.dot(start_to_center, line_vec) / np.dot(line_vec, line_vec) * line_vec
+    
+    # Closest point on the line to the circle center
+    closest_point = np.array(line_start) + projection
+    
+    # Distance between closest point and circle center
+    distance = np.linalg.norm(closest_point - circle_center)
+    
+    # If the distance is less than or equal to the circle radius, there's an intersection
+    if distance <= circle_radius:
+        return True
+    else:
+        return False
+    
 img = cv2.imread('assets/martin.jpg')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
@@ -53,6 +75,7 @@ print(len(filtered_lines))
 for lin in filtered_lines:
     x1,y1,x2,y2,rho,theta = lin
     sorted_lines = sorted(filtered_lines, key=lambda x: x[4], reverse=True)
+    
     for lin in sorted_lines:
         x1, y1, x2, y2, rho, theta = lin
         cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
@@ -60,8 +83,42 @@ for lin in filtered_lines:
 print("number of lines:", len(sorted_lines))
 #split into 5 lines
 split_lines = [sorted_lines[i:i+5] for i in range(0, len(sorted_lines), 5)]
-cv2.circle(img, (300,300),9,(0,0,255),-1)
-print("\n\n",split_lines)
+
+value_to_add = 1
+for sublist in split_lines:
+    for index, tup in enumerate(sublist):
+        sublist[index] = tup + (value_to_add,)
+        value_to_add += 1
+        if value_to_add > 5:
+            value_to_add = 1
+print("split_lines:", split_lines)
+circles = []
+circle1 = (300,330,18)
+cv2.circle(img, (circle1[0],circle1[1]),circle1[2],(0,0,255),-1)
+circles.append((circle1[0],circle1[1],circle1[2]))
+
+
+for circle in circles:
+    (x_c,y_c,r) = circle
+    for line in split_lines:
+        for x1,y1,x2,y2,rho,theta, note_pos in line:
+            intersect = line_circle_intersection((x1,y1),(x2,y2),(x_c,y_c),r)
+            if intersect:
+                print(f"intersection at note_pos {note_pos}")
+                if note_pos == 1:
+                    print("note is a E")
+                elif note_pos == 2:
+                    print("note is a G")
+                elif note_pos == 3:
+                    print("note is a B")
+                elif note_pos == 4:
+                    print("note is a D")
+                elif note_pos == 5:
+                    print("note is a F")
+            else:
+                print("no intersection")
+
+# print("\n\n",split_lines)
 cv2.namedWindow("hough", cv2.WINDOW_NORMAL) #så ikke zoomed-in
 cv2.resizeWindow("hough", img.shape[0]-200, img.shape[1]-130) 
 cv2.imshow('hough',img)
